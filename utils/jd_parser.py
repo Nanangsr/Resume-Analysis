658dc1834e2d8be2a1dc913933c514d6ba6f51c0
Nanangsr <nanangsafiur@gmail.com>
2025-05-13 21:12:22 +0700

    Update jd_parser.py

--- utils/jd_parser.py
+++ utils/jd_parser.py
@@ -2,5 +2,5 @@ from utils.resume_parser import parse_resume
 from typing import Tuple
 
 def parse_jd(file) -> Tuple[str, str]:
-    """Extract text from job description file"""
+    """Ekstrak teks dari file deskripsi pekerjaan (job description)"""
     return parse_resume(file, file.name if hasattr(file, 'name') else "JD File") 
